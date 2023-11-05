ab2f74a08c67774662f5aeb1c5a9a882c3f5773d
tanish vashisth <tanishvashisth@gmail.com>
2023-11-06 00:47:44 +0530

    nothing

--- market.py
+++ market.py
@@ -2,19 +2,18 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/market.db'
 db = SQLAlchemy(app)
 
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(length=20), nullable= False, unique= True )
-    barcode = db.Column(db.String(length=12), nullable=False, unique =True)
+    name = db.Column(db.String(20), nullable=False, unique=True)
+    barcode = db.Column(db.String(12), nullable=False, unique=True)
     price = db.Column(db.Integer(), nullable=False)
-    description = db.Column(db.String(length=1024), nullable = False, unique = True)
+    description = db.Column(db.String(1024), nullable=False, unique=True)
 
     def __repr__(self):
         return f'Item {self.name}'
-
 
 @app.route("/")
 @app.route('/home')
@@ -26,6 +25,7 @@ def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
 
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
