9f15b74907010d6ddb482fcb709ddb5fd54f031b
Prome-theus <tanishvashisth@gmail.com>
2023-11-07 00:17:58 +0530

    structured

--- app.py
+++ app.py
@@ -1,35 +1,5 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from market import app
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
-db = SQLAlchemy(app)
-
-class Item(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(20), nullable=False, unique=True)
-    barcode = db.Column(db.String(12), nullable=False, unique=True)
-    price = db.Column(db.Integer(), nullable=False)
-    description = db.Column(db.String(1024), nullable=False, unique=True)
-
-    def __repr__(self):
-        return f'Item {self.name}'
-
-@app.route("/")
-@app.route('/home')
-def home_page():
-    return render_template("home.html")
-
-@app.route("/market")
-def market_page():
-    items = Item.query.all()
-    return render_template('market.html', items=items)
-
-
-
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-
+if __name__ == '__main__':
     app.run(debug=True)
 
