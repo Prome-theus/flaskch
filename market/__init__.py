a0a8f7566eb85654d234fb4922a85064538fc821
Prome-theus <tanishvashisth@gmail.com>
2023-11-08 23:27:50 +0530

    registration

--- market/__init__.py
+++ market/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = '6e6cf3f875a3a73830d88caf'
 db = SQLAlchemy(app)
 
 from market import routes
