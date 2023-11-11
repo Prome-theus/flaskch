5919ce6fed30b0d0ffd5dab33fc84000026d2bf3
Prome-theus <tanishvashisth@gmail.com>
2023-11-11 23:22:21 +0530

    flash error

--- market/models.py
+++ market/models.py
@@ -1,12 +1,14 @@
 from market import db
 
+
 class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=38), unique=True, nullable=False)
     email = db.Column(db.String(length=50), nullable=False, unique=True)
     password = db.Column(db.String(length=60), nullable=False)
     budget = db.Column(db.Integer(), nullable=False, default=1000)
-    items = db.relationship('Item', backref ='owned_user', lazy=True)
+    items = db.relationship('Item', backref='owned_user', lazy=True)
+
 
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
