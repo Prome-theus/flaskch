b6808568fa2c2782ad67ee51d2c215b364ed493a
tanish vashisth <tanishvashisth@gmail.com>
2023-11-16 21:11:05 +0530

    log out and login

--- market/routes.py
+++ market/routes.py
@@ -1,8 +1,9 @@
 from market import app
 from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
-from market.forms import RegisterForm
+from market.forms import RegisterForm, LoginForm
 from market import db
+from flask_login import login_user, logout_user
 
 
 @app.route('/')
@@ -35,3 +36,24 @@ def register_page():
         for err_msg in form.errors.values():
             flash(f'There was an error with creating your account: {err_msg}',category='danger')
     return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(attempted_password=form.password.data):
+            login_user(attempted_user)  
+            flash('Success! You are logged in as:  {attempted_user.username}', category='success')
+            return redirect(url_for('market_page'))
+        else:
+            flash('Username and Password does not exist', category='danger')
+
+    return render_template("login.html", form=form)
+    
+app.route('/logout')
+def logout_page():
+    logout_user()
+    flash("you have been logged out!", category='info')
+    return redirect(url_for("home_page"))
