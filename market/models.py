ae7e494df2604773b8a3121699e392522ff15f5c
Prome-theus <tanishvashisth@gmail.com>
2023-12-02 01:42:44 +0530

    few minor changes

--- market/models.py
+++ market/models.py
@@ -19,9 +19,9 @@ class User(db.Model, UserMixin):
     @property
     def prettier_budget(self):
         if len(str(self.budget)) >= 4:
-            return f'{str(self.budget)[:-3]},{str(self.budget)[-3:]}$'
+            return f'₹ {str(self.budget)[:-3]},{str(self.budget)[-3:]}'
         else:
-            return f"{self.budget}$"
+            return f"₹ {self.budget}$"
 
     @property
     def password(self):
