6c6eac271f9cea78e9260a3fd74a30906fac539e
tanish vashisth <tanishvashisth@gmail.com>
2023-12-22 18:06:05 +0530

    hoi hoi

--- market/__init__.py
+++ market/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_oauthlib.client import OAuth
 
 
 app = Flask(__name__)
@@ -14,5 +15,13 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view="login_page"
 login_manager.login_message_category="info"
+
+oauth = OAuth(app)
+
+google = oauth.remote_app(
+    'google',
+    consumer_key = '1094737348611-ql5ga54uf68h1bkhf6mulbhvomemsg13.apps.googleusercontent.com',
+    consumer_secret = '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',
+)
 
 from market import routes
--- market/routes.py
+++ market/routes.py
@@ -1,10 +1,27 @@
 from market import app
-from flask import render_template, redirect, url_for, flash, request
+from flask import render_template, redirect, url_for, flash, current_user
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForm , ItemAdd
+from flask_oauthlib.client import OAuth
 from market import db
 from flask_login import login_user, logout_user, login_required
 
+
+oauth = OAuth(app)
+google = oauth.remote_app(
+    'google',
+    consumer_key='1094737348611-ql5ga54uf68h1bkhf6mulbhvomemsg13.apps.googleusercontent.com',
+    consumer_secret='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',
+    request_token_params={
+        'scope': 'email',
+    },
+    base_url='https://www.googleapis.com/oauth2/v1/',
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    redirect_uri='http://localhost:5000/login/authorized/google',
+)
 
 @app.route('/')
 @app.route('/home')
@@ -61,6 +78,10 @@ def register_page():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login_page():
+    if current_user.is_authenticated:
+        flash('you are already logged in', 'info')
+        return redirect(url_for('home_page'))
+
     form = LoginForm()
     if form.validate_on_submit():
         attempted_user = User.query.filter_by(username=form.username.data).first()
@@ -72,6 +93,34 @@ def login_page():
             flash('Username and Password does not exist', category='danger')
 
     return render_template("login.html", form=form)
+
+@app.route('login/authorized')
+def authorized():
+    response = google.authorized_response()
+    if response is None or response.get('access_token') is None:
+        flash('Access denied: Reason={} error={}'.format(
+            request.args['error_reason'],
+            request.args['error_description']
+        ))
+        return redirect(url_for('home_page'))
+    
+    access_token = response['access_token']
+    user_info = google.get('userinfo')
+
+    if current_user.is_authenticated:
+        current_user.google_id = user_info.data['id']
+        db.session.commit()
+        flash('successful Linked google account to your profile', 'success')
+        return redirect(url_for('home_page'))
+    else:
+        user = User.query.filter_by(google_id=user_info.data[id]).first()
+        if user:
+            login_user(user)
+            flash(f'Success! you are logged in as: {user.username}', 'success')
+            return redirect(url_for('home_page'))
+        else:
+            flash('google account not linked to any existing user', 'danger')
+            return redirect(url_for('home_page'))
     
 @app.route('/logout')
 def logout_page():
@@ -104,3 +153,4 @@ def Item_add():
             flash(f'There was an error with adding the item: {err_msg}',category='danger')
         
     return render_template('itemadd.html', form=form)
+
